f33389bdfcc0ee570fe2b96f1d442b1fc3b9e7e9
Elisa Mussumeci <elisamussumeci@gmail.com>
2016-03-15 23:30:08 -0300

    Add time series
    Fix #5

---
+++ static/js/time-series.js
@@ -0,0 +1,40 @@
+$(function () {
+    $.getJSON('/api/timeseries', function (data) {
+
+        final_data = [];
+        for (var i in data) {
+            final_data.push([Date.parse(i), data[i]]);
+        }
+
+        $('#time-series').highcharts('StockChart', {
+            chart: {
+                zoomType: 'x',
+                renderTo: 'time-series'
+            },
+            title: {
+                text: 'Série Temporal'
+            },
+            subtitle: {
+                text: document.ontouchstart === undefined ?
+                        'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+            },
+            legend: {
+                enabled: true,
+                layout: 'vertical',
+                align: 'left',
+                x: 120,
+                verticalAlign: 'top',
+                y: 80,
+                floating: true,
+                backgroundColor: '#FFFFFF'
+            },
+            series: [{
+                name: 'Total de artigos',
+                data: final_data,
+                color: '#4572A7',
+                dashStyle: 'Solid',
+                animation: true
+            }]
+        });
+    });
+});
